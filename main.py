bb59f047f300e073487cf53b0a85c1132fe40d02
anna <annalin.design@gmail.com>
2022-09-27 08:21:15 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, validators
+from wtforms.validators import DataRequired
+from projs import project_list
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+    time = datetime.now().hour
+    return render_template("index.html", time=time, project_list=project_list)
+
+
+@app.route("/projects/<int:proj_num>")
+def projects(proj_num):
+    total_proj = len(project_list)
+    return render_template(f"{proj_num}.html", project_list=project_list, project_id=proj_num, total_proj=total_proj)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ projs.py
@@ -0,0 +1,34 @@
+project_list = [
+                  {
+                      "Title": "AZURE Gift Campaign",
+                      "Subtitle": "A digital campaign to promote gift subscriptions.",
+                      "Thumbnail": "static/images/az-campaign-2.gif",
+                      "id": 0
+                  }
+    ,
+                  {
+                      "Title": "Azure Magazine",
+                      "Subtitle": "Select editorial pages for Azure Magazine",
+                      "Thumbnail": "static/images/editorial.jpg",
+                      "id": 1,
+                  }
+,
+
+              {
+                  "Title": "AZ Awards",
+                  "Subtitle": "Annual video to promote the awards.",
+                  "Thumbnail": "static/images/thumb-aza.gif",
+                  "id": 2
+              }
+,
+              {
+                  "Title": "Branded",
+                  "Subtitle": "Creating a memorable in-person conference experience",
+                  "Thumbnail": "static/images/thumb-branded.jpg",
+                  "id": 3,
+              }
+
+
+
+]
+
