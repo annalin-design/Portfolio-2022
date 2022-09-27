e5dae55a7d01ba6749f28db2d199c76d0b129367
anna <annalin.design@gmail.com>
2022-09-27 15:22:04 -0400

    Initial commit

--- projs.py
+++ projs.py
@@ -1,29 +1,29 @@
 project_list = [
                   {
-                      "Title": "AZURE Gift Campaign",
-                      "Subtitle": "A digital campaign to promote gift subscriptions.",
+                      "Title": "Give the gift of Azure",
+                      "Subtitle": "A new perspective on magazine gift subscriptions during the pandemic.",
                       "Thumbnail": "static/images/az-campaign-2.gif",
                       "id": 0
                   }
     ,
                   {
-                      "Title": "Azure Magazine",
-                      "Subtitle": "Select editorial pages for Azure Magazine",
+                      "Title": "Azure Magazine Editorial",
+                      "Subtitle": "Select editorial pages designed for the awarding-winning magazine.",
                       "Thumbnail": "static/images/editorial.jpg",
                       "id": 1,
                   }
 ,
 
               {
-                  "Title": "AZ Awards",
-                  "Subtitle": "Annual video to promote the awards.",
+                  "Title": "AZ Awards Call for Submissions Video",
+                  "Subtitle": "Inviting architects and designers to join an international competition.",
                   "Thumbnail": "static/images/thumb-aza.gif",
                   "id": 2
               }
 ,
               {
-                  "Title": "Branded",
-                  "Subtitle": "Creating a memorable in-person conference experience",
+                  "Title": "Branded Youth Entrepreneurship Conference",
+                  "Subtitle": "Crafting a memorable visual experience.",
                   "Thumbnail": "static/images/thumb-branded.jpg",
                   "id": 3,
               }
